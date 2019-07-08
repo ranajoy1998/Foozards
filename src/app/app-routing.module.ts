f40803d707a16682515f51bc5f7477a1b7413729
ranajoy1998 <ranajoyhajra@gmail.com>
2019-07-08 10:28:51 +0530

    Attemping to correct navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'home', component: HomeComponent
+},
+{
+  path: 'about', component: AboutComponent
+},
+{
+  path: 'contact', component: ContactComponent
+},
+{
+  path: 'login', component: LoginComponent
+},
+{
+  path: 'singup', component: SignupComponent
+},
+{
+  path: '', redirectTo: '/home', pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
