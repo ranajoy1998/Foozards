71c661bce5d9b8a75bb4a33ab505a5fe0747af68
ranajoy1998 <ranajoyhajra@gmail.com>
2019-09-06 00:12:57 +0530

    First full prod version complete - Foozards v0.0

--- src/app/user-profile/profile/profile.component.ts
+++ src/app/user-profile/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingService } from 'src/app/shared/booking.service';
+import { Booking } from 'src/app/shared/booking.model';
 
 @Component({
   selector: 'app-profile',
@@ -6,13 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  custemail = localStorage.getItem("cemail");
 
-  constructor() { }
+  constructor(private bookingService: BookingService) { }
 
   ngOnInit() {
   }
 
   getCname() {
     return localStorage.getItem("cname");
+  }
+
+  getCphone() {
+    return localStorage.getItem("cphone");
+  }
+
+  getCemail() {
+    return localStorage.getItem("cemail");
+  }
+
+  getCaddress() {
+    return localStorage.getItem("caddress");
+  }
+
+  getCorderno() {
+    this.bookingService.getBookings(this.custemail).subscribe(
+      res=>{
+        this.bookingService.bookings = res as Booking[];
+      },
+      err=>{
+        console.log(err);
+      }
+    );
+    return this.bookingService.bookings.length;
   }
 }
